f80829c9aa912dbc67f5503536c69a50fd9c001e
catnapwastaken <catnaptaken.meow@gmail.com>
2025-07-24 22:39:10 +0300

    Update auto_rejoin_main_script.lua

--- auto_rejoin_main_script.lua
+++ auto_rejoin_main_script.lua
@@ -4,7 +4,7 @@ local player = Players.LocalPlayer
 
 local farmScriptURL = "https://gist.githubusercontent.com/catnapwastaken/185700fbddde0d2253237c8c73e61d9d/raw/75156dfcbbd975bab73eee00c5ce5d4e77e07cce/MM2%2520auto%2520farm"
 
-queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/catnapwastaken/simple-mm2-farm-script-auto-load-etc/refs/heads/main/auto%20rejoin%20main%20script"))()')
+queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/catnapwastaken/simple-mm2-farm-script-auto-load-etc/refs/heads/main/auto_rejoin_main_script.lua"))()')
 
 game:GetService("CoreGui"):WaitForChild("RobloxPromptGui").PromptDisconnected.DescendantAdded:Connect(function()
     task.wait(2)
